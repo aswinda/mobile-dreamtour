0d6ae228b51ef9faf0c92e0912a01b4d1039dc9e
Aswinda Prima Putra <aswinda.pp@gmail.com>
2016-05-23 02:15:44 +0700

    Add Android Platform

---
+++ platforms/android/assets/www/angular/controller/regularPackagesController.js
@@ -0,0 +1,32 @@
+dreamtour.controller('RegularPackagesController', ['$scope', '$http','cfpLoadingBar', 
+  function RegularPackagesController($scope, $http, cfpLoadingBar) 
+{
+    $scope.packages = {};
+    $scope.package = {};
+    $scope.state = 'list-regular';
+    // Get Packages
+    $http(
+    {
+        url     : host +  "getRegularPackages",
+        method  : "GET",
+    })
+    .success(function(data)
+    {
+        $scope.packages = data;
+        //console.log($scope.packages);
+    })
+    .error(function (error)
+    {
+        console.log(error);
+    });
+
+    $scope.detail = function(index)
+    {
+        $scope.package = $scope.packages[index];
+        $scope.state = 'detail';
+
+        console.log($scope.package);
+    }
+
+
+}]);
